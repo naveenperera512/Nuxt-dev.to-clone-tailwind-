d950917c90c69408918fd9e050c6e5b82f958823
naveen10qbit <naveen@10qbit.com>
2021-09-03 18:37:45 +0530

    build

---
+++ pages/articles/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, $axios }) {
+    const article = await $axios.$get('https://dev.to/api/articles/721780')
+    return { article }
+  }
+}
+</script>
